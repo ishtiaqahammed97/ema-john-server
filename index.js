c0ac216dd47ab65bb6181b37857cab3637ab6097
ishtiaq <isah2080@ymail.com>
2023-04-21 14:25:39 +0600

    vercel.json file added

--- index.js
+++ index.js
@@ -22,45 +22,46 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const productsCollection = client.db("emaJohnStore").collection("products");
   const ordersCollection = client.db("emaJohnStore").collection("oders");
-  
-    app.post('/addProduct', (req, res) => {
-        const products = req.body;
-        productsCollection.insertOne(products)
-        .then(result => {
-            console.log(result.insertedCount);
-            res.send(result.insertedCount)
-        })
-    })
 
-    app.get('/products', (req, res) => {
-      const search = req.query.search;
-      productsCollection.find({name: {$regex : search} })
-      .toArray( (err, documents) => {
-          res.send(documents);
+  app.post('/addProduct', (req, res) => {
+    const products = req.body;
+    productsCollection.insertOne(products)
+      .then(result => {
+        console.log(result.insertedCount);
+        res.send(result.insertedCount)
       })
-    })
+  })
 
-    app.get('/product/:key', (req, res) => {
-      productsCollection.find({key : req.params.key})
-      .toArray( (err, documents) => {
-          res.send(documents[0]);
-      })
-    })
-
-    app.post('/productsByKeys', (req, res) => {
-      const productKeys = req.body;
-      productsCollection.find({key: { $in:productKeys}})
-      .toArray( (err, documents) => {
+  app.get('/products', (req, res) => {
+    const search = req.query.search;
+    productsCollection.find({ name: { $regex: search } })
+      .toArray((err, documents) => {
         res.send(documents);
       })
-    })
+  })
 
-    app.post('/addOrder', (req, res) => {
-      const order = req.body;
-      ordersCollection.insertOne(order)
+
+  app.get('/product/:key', (req, res) => {
+    productsCollection.find({ key: req.params.key })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
+  })
+
+  app.post('/productsByKeys', (req, res) => {
+    const productKeys = req.body;
+    productsCollection.find({ key: { $in: productKeys } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  app.post('/addOrder', (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order)
       .then(result => {
-          console.log(result.insertedCount);
-          res.send(result.insertedCount > 0)
+        console.log(result.insertedCount);
+        res.send(result.insertedCount > 0)
       })
   })
 
